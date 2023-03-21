a4fe79dba19cfdc0dc7324444634631ebbfda54c
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2023-03-21 14:08:07 -0300

    Create a maven project with JUnit5 support

---
+++ Source_Code/01/calculator-with-maven/src/test/java/FirstTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    public void testJUnit5Support(){
+        System.out.println("Hello, cruel JUnit 5 (Jupiter) world !");
+    }
+}
