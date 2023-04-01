671892b62d4537ba5eac7f395f4c3baf97ef8ed4
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2023-04-01 19:39:20 -0300

    Improve previous example by outputing a message when the test fails

--- Source_Code/03/calculator/src/test/java/org/example/junit5/CalculatorTest.java
+++ Source_Code/03/calculator/src/test/java/org/example/junit5/CalculatorTest.java
@@ -5,14 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
     @Test
     void integerDivision() {
         // Arrange
         Calculator calculator = new Calculator();
+
         // Act
         int result = calculator.integerDivision(10, 5);
-        // Assert
-        assertEquals(2, result);
+
+        // Assert (first way)
+        assertEquals(2, result, "4/2 didn't produce 2 as expected");
+
+        // Assert (second way)
+        // if (result != 2)
+        //   fail("4/2 didn't produce 2 as expected");
     }
 }
